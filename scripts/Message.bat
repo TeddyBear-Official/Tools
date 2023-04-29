3b7f02174bc51e7fe0c2c2a963a80cf392b30fab
PasteBinNotAvailable <132144508+PasteBinNotAvailable@users.noreply.github.com>
2023-04-30 00:23:56 +1000

    Update Message.bat

--- scripts/Message.bat
+++ scripts/Message.bat
@@ -1,6 +1,6 @@
 @echo off
 title Message System
-echo Use the * sign for everybody on your internet signal.
+echo Use the * sign for everybody on your local network.
 set /p messageUser="User: "
 set /p message="Message: "
 msg %messageUser% %message%
