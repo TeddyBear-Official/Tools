e8f33b4e8d95f3e08eb85cff5aaa631e9d773bba
PasteBinNotAvailable <132144508+PasteBinNotAvailable@users.noreply.github.com>
2023-04-30 00:31:43 +1000

    Update taskkill.bat

--- scripts/taskkill.bat
+++ scripts/taskkill.bat
@@ -1,6 +1,6 @@
 @echo off
 @title taskkill cmd process
-
+start Main.bat
 set /p taskkill="Program to kill: "
 taskkill /f /im %taskkill%
 echo Task successfully killed with all parents gone with it.
