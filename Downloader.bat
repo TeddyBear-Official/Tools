ae905279235c01188c63697060b03039b6f7c4b4
PasteBinNotAvailable <132144508+PasteBinNotAvailable@users.noreply.github.com>
2023-04-30 00:37:23 +1000

    Update and rename uploader to Downloader.bat

---
+++ Downloader.bat
@@ -0,0 +1,34 @@
+@echo off
+title Downloader
+
+:Download
+if not exist scripts ( mkdir scripts )
+if not exist settings ( mkdir settings )
+
+if not exist scripts\Message.bat (
+set fileLink=https://raw.githubusercontent.com/TeddyBear-Official/Tools/main/scripts/Message.bat
+set fileName=Message.bat
+goto InstallScript
+)
+
+if not exist Main.bat (
+set fileLink=https://raw.githubusercontent.com/TeddyBear-Official/Tools/main/scripts/Main.bat
+set fileName=Main.bat
+goto InstallGeneral
+)
+
+if not exist scripts\taskkill.bat (
+set fileLink=https://raw.githubusercontent.com/TeddyBear-Official/Tools/main/scripts/taskkill.bat
+set fileName=taskkill.bat
+goto InstallScript
+)
+
+exit
+
+:InstallScript
+powershell -Command "Invoke-WebRequest %fileLink% -Outfile scripts\%fileName%"
+goto Download
+
+:InstallGeneral
+powershell -Command "Invoke-WebRequest %fileLink% -Outfile %fileName%"
+goto Download
