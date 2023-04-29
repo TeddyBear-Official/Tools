102d15f0e5dac519cb27a27db3a9f0aea7eab025
PasteBinNotAvailable <132144508+PasteBinNotAvailable@users.noreply.github.com>
2023-04-30 00:29:38 +1000

    Update taskkill.bat

--- scripts/taskkill.bat
+++ scripts/taskkill.bat
@@ -1,7 +1,7 @@
 @echo off
 @title taskkill cmd process
 
-echo /p taskkill="Program to kill: "
+set /p taskkill="Program to kill: "
 taskkill /f /im %taskkill%
 echo Task successfully killed with all parents gone with it.
 pause
